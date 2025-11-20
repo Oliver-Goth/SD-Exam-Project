ce38e5e67616e0a47cc2ce022960ac310347463c
ErminDZ <ermin@outlook.dk>
2025-11-20 13:37:22 +0100

    Added CreateUser

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import CreateUser from './pages/createUser.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <CreateUser />
   </StrictMode>,
 )
---
+++ frontend/src/pages/createUser.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { DatePicker } from "@fluentui/react-datepicker-compat";
+import { Field, Input, Body1, Button, Card, CardFooter, CardHeader } from "@fluentui/react-components";
+import '../index.css';
+
+const createUserPage = () => (
+  <div 
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      padding: '20px',
+    }}
+  >
+    <Card style={{ width: '500px', padding: '20px' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '16px' }}>
+        <CardHeader
+          header={<Body1><b>Create User</b></Body1>}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '12px', marginTop: '0px' }}>
+        <Field label="E-mail"><Input className="myInput" /></Field>
+        <Field label="Username"><Input className="myInput" /></Field>
+        <Field label="Password"><Input className="myInput" /></Field>
+        
+        <div style={{ display: 'flex', gap: '12px' }}>
+          <Field label="First Name" style={{ flex: 1 }}>
+            <Input className="myInput" />
+          </Field>
+          <Field label="Last Name" style={{ flex: 1 }}>
+            <Input className="myInput" />
+          </Field>
+        </div>
+        
+        <Field label="Phone Number"><Input className="myInput" /></Field>
+        <Field><DatePicker placeholder="Select a date..." /></Field>
+      </div>
+      <CardFooter style={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
+        <Button>Reply</Button>
+      </CardFooter>
+    </Card>
+  </div>
+);
+
+export default createUserPage;
