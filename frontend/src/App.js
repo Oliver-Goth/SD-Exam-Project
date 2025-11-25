ac47766524422cc5fe343bbaf5a2d669afb4badb
Oliver-Goth <Olivergoth00@gmail.com>
2025-11-25 11:48:25 +0100

    backend update

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>MTOGO Frontend</h1>
-      <p>Frontend is running</p>
-    </div>
-  );
-}
